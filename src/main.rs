8c952f60e1861da3ea1aaccdaba566cd6fec475a
Levy Barbosa <marcio.levy20@gmail.com>
2024-10-23 22:26:24 -0300

    tokio

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,37 @@ fn main() -> anyhow::Result<()> {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    // eventfd is needed by our mio poll implementation.  Note you should set max_fds
+    // higher if you have other code that may need eventfd.
+    log::info!("Setting up eventfd...");
+    let config = esp_idf_svc::sys::esp_vfs_eventfd_config_t {
+        max_fds: 1,
+        ..Default::default()
+    };
+    esp_idf_svc::sys::esp! { unsafe { esp_idf_svc::sys::esp_vfs_eventfd_register(&config) } }?;
 
+    log::info!("Setting up board...");
     let peripherals = Peripherals::take()?;
+    let led = PinDriver::output(peripherals.pins.gpio2)?;
 
-    let mut led = PinDriver::output(peripherals.pins.gpio2)?;
-    
+    log::info!("Starting async run loop");
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?
+        .block_on(async move {
+            tokio::spawn(blink_led_loop(led)).await?
+        })?;
+
+    Ok(())
+}
+
+async fn blink_led_loop<'d, T: Pin>(mut led: PinDriver<'d, T, Output>, ) -> anyhow::Result<()> {
     loop {
         log::info!("Setting LED high");
         led.set_high()?;
-        // we are sleeping here to make sure the watchdog isn't triggered
-        FreeRtos::delay_ms(1000);
-
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         log::info!("Setting LED low");
         led.set_low()?;
-        FreeRtos::delay_ms(1000);
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
     }
 }
